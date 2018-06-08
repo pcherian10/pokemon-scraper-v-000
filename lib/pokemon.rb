0a52427bf7102595298ec73e2289a4379b4c8565
Paul <pcherian10@gmail.com>
2018-06-08 11:53:11 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,35 +20,25 @@ end
 
 
 =begin
-
-
 class Cat
-
   @@all = []
-
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
     @@all << self
   end
-
   def self.all
     @@all
   end
-
   def self.save(name, breed, age, database_connection)
     database_connection.execute("INSERT INTO cats (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
   end
 end
-
 database_connection = SQLite3::Database.new('db/pets.db')
-
 Cat.new("Maru", "scottish fold", 3)
 Cat.new("Hana", "tortoiseshell", 1)
-
 Cat.all.each do |cat|
   Cat.save(cat.name, cat.breed, cat.age, database_connection)
 end
-
 =end
